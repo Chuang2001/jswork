0e688e6534a60a6732cb762b395c8dd0331aead5
Chuang2001 <55181006+Chuang2001@users.noreply.github.com>
2019-10-16 19:34:52 +0800

    Add files via upload

--- work6/pai.js
+++ work6/pai.js
@@ -1,33 +1,36 @@
 function calculate(){
-    let num = document.getElementById('text').value
+    let num = document.getElementById('num').value
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById('pai').value =pi(Text)
+        document.getElementById('pai').value =pi(num)
     }
 }
 function factorial(n){
     let sum = 1
     //for循环累乘
-    for(let i=1;i>0;++i){
-        sum =i*i
+    for(let i=1;i<=n;++i){
+        sum *=i
     }
     return sum
 }
+     //for循环累乘
 function oddFactorial(n){
     let sum = 1
-    for(let i=1;i>0;++i){
-        sum =i*(2*i+1)
+    for(let i=1;i<=n;++i){
+        sum *=(2*i+1)
     }
     return sum  
 }
+     //累加运算
 function pi(num){
     let sum = 0
     for(let i=0;i<=num;++i){
-        let dividend = factorial( )
-        let diisor = oddFactorial(n)
-        sum += n
+        let dividend = factorial(i )
+        let divisor = oddFactorial(i)
+        sum +=(dividend/divisor)
     }
-    return sum
+    //返回π/2后*2输出
+    return sum*2
 }
 
 calculate()
--- work6/tarray.js
+++ work6/tarray.js
@@ -1,16 +1,17 @@
-//创建数组
-var arr = [1,2,3,4,2,5,6,2,7,2];
+//创建副本
+var  arr =[1,2,3,4,2,5,6,2,7,2];
 //显示数组
-document.getElementById('array').innerHTML = arr.toString()
+document.getElementById('array').innerHTML =arr.toString()
 //index表示新数组newarr的下标，默认为0
-var index = 0,newArr = [];
+var index = 0, newArr = [];
 //遍历数组
-for(var i in arr){
-    //数组元素不等于2，则将其保存到newarr中
-    if(arr[i] != 2);{
-    newArr[index] = arr[i];
-    ++index;
-  }
+for(var i in arr ){
+  //数组元素不等于2，将其保存到newarr中
+    if (arr[i] !=2){
+        newArr[index] = arr[i];
+        ++index;
+
+    }
 }
 //显示提取后的数组
-document.getElementById('transArray').innerHTML = newArr.toString()
+ document.getElementById('transArray').innerHTML = newArr.toString()

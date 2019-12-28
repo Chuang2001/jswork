5078a7c86e2da45df08ee8d73575f32806be3a51
Chuang2001 <55181006+Chuang2001@users.noreply.github.com>
2019-12-28 21:38:10 +0800

    Add files via upload

--- work18/index.js
+++ work18/index.js
@@ -8,7 +8,7 @@ function send(){
     }else{
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
     }
-    // xmlhttp.open('post', 'http://localhost:8080/ajax', true)//测试通过后本行注释
+    //xmlhttp.open('post', 'http://localhost:8080/ajax', true)//测试通过后本行注释
     xmlhttp.open('post', 'http://139.9.81.203:8090/ajax', true)//测试通过后本行取消注释
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     xmlhttp.send('sno=' + sno + '&name=' + name+"&content="+content)
--- work19/index.js
+++ work19/index.js
@@ -1,20 +1,20 @@
-let ws
+let ws 
 function connect() {
-    let server = document.getElementById('server').value
+    let server = document.getElementById('server').value 
     ws = new WebSocket(server);
-    ws.onopen = function(){
-        document.getElementById('conn').disabled='disable';
-        document.getElementById('disconn').disabled='';
-        let nickname = document.getElementById('nickname').value
-        if(nickname){
-            ws.send('nickname|' + nickname)
-        }
-    }    
+    ws.onopen = function() {
+       document.getElementById('conn').disabled='disabled';
+       document.getElementById('disconn').disabled='';
+       let nickname=document.getElementById('nickname').value
+       if(nickname){
+           ws.send('nickname|'+nickname)
+       } 
+    }
     ws.onclose = function(){
         document.getElementById('conn').disable='';
-        document.getElementById('disconn').disable = 'disable';
+        document.getElementById('disconn').disable='disable';
     }
-    ws.onmessage = function(event){
+    ws.onmessage=function(){
         let board = document.getElementById('board')
         let newmsg = document.createElement('div')
         console.log(event.data)
@@ -22,12 +22,14 @@ function connect() {
         board.appendChild(newmsg)
         board.scrollTop = board.scrollHeight;
     }
-}
+} 
+
 function disconnect(){
     ws.close()
 }
 
 function send(){
     let msg = document.getElementById('content').value
-     ws.send(msg)
+    ws.send(msg)
 }
+       
